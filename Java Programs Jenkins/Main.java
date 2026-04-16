0c2d8e902aefea18ece4b0cf951baf51417f9c4f
Shivaram-ch123 <shivaram.c@techouts.co.in>
2026-04-16 13:09:17 +0530

    Add files via upload
    updated folders

---
+++ Java Programs Jenkins/Main.java
@@ -0,0 +1,11 @@
+class Main{
+    public static void main(String args[]){
+        
+        int temp=10;
+        while(temp > 0){
+            System.out.println("hi");
+            temp-=2;
+        }
+
+}
+}
