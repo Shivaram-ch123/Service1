fde67aeadeee88ea9076d4e1ebab7b7356a54842
Shivaram-ch123 <shivaram.c@techouts.co.in>
2026-04-16 15:08:02 +0530

    Add files via upload
    added sample java program

---
+++ Java Programs Jenkins/SampleProgram.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+
+
+class SampleProgram{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int temp = sc.nextInt();
+        while(temp>0){
+            System.err.println("hello how are you");
+            temp--;
+
+        }
+    }
+}
